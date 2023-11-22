d7bafcd66121ea0d9cb5b68d203fa04cbe45fe5f
Kat <katarzynaa89@gmail.com>
2023-11-22 17:20:25 +0000

    Demonstration03 Project set up

---
+++ Unit_Testing_peer_Classes/lib/diary_entry_formatter.py
@@ -0,0 +1,9 @@
+class DiaryEntryFormatter:
+    def __init__(self, diary_entry):
+        # diary_entry is an instance of DiaryEntry
+        pass
+
+    def format(self):
+        # Returns a nicely formatted diary entry like this:
+        #   My Title (2 words): My Contents
+        pass
---
+++ Unit_Testing_peer_Classes/tests/test_diary_entry_formatter.py
@@ -0,0 +1,15 @@
+from lib.diary_entry_formatter import DiaryEntryFormatter
+from unittest.mock import Mock
+
+def test_formats_a_diary_entry():
+    fake_diary_entry = Mock()
+    
+    # To set properties on the mock, we can just set them
+    fake_diary_entry.title = "Hello"
+    fake_diary_entry.contents = "Hello, world!"
+
+    # And we've seen how to tell the mock to return a value for a method
+    fake_diary_entry.word_count.return_value = 2
+
+    formatter = DiaryEntryFormatter(fake_diary_entry)
+    assert formatter.format() == "Hello (2 words): Hello, world!"
